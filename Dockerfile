1239cd341518abd55cdb9a40f471192c01ef1ba0
hualeel <hualeel@sina.com>
2020-12-14 16:21:21 +0800

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,24 @@
 FROM registry.cn-qingdao.aliyuncs.com/biz-express/apline-python:v20181229
 MAINTAINER Henry li
 
-ENV APP_DIR=appdir \
-EE_PYPI=https://mirrors.aliyun.com/pypi/simple/ \
-EE_API_SERVICE_PORT=5000
+# 环境变量
+ENV SERVICE_NAME=ovn_demo_service \
+SERVICE_PORT=5000 \
+SERVICE_DIR=$SERVIE_NAME \
+PYPI=https://mirrors.aliyun.com/pypi/simple/
 
 # 创建工作目录
-RUN mkdir -p /$APP_DIR
-COPY . /$APP_DIR
-WORKDIR /$APP_DIR
+RUN mkdir -p /$SERVICE_DIR
+COPY . /$SERVICE_DIR
+WORKDIR /$SERVICE_DIR
 
 # 添加apk国内源，安装扩展包
 RUN echo "http://mirrors.aliyun.com/alpine/v3.8/main/" > /etc/apk/repositories && \
     echo "http://mirrors.aliyun.com/alpine/v3.8/community" >> /etc/apk/repositories && \
-    pip install -i $EE_PYPI --upgrade pip && \
-    pip install -i $EE_PYPI -r requirements.txt && \
+    pip install -i $PYPI --upgrade pip && \
+    pip install -i $PYPI -r requirements.txt && \
     chmod 700 ./run.sh
 
 # 端口
-EXPOSE $EE_API_SERVICE_PORT
+EXPOSE $SERVICE_PORT
 CMD sh ./run.sh
--- run.sh
+++ run.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
 echo " ----------EE Express RESRfull API Devp--------------"
-NAME=$SERVICE_NAME
-PORT=$SERVICE_PORT  # 600 0
+SERVICE_NAME=$SERVICE_NAME  # kube_ovn_demo_service
+SERVICE_PORT=$SERVICE_PORT  # 6000
+SERVICE_DIR=$SERVICE_DIR    # app
 
-echo "parameter:$NAME"
-count=`ps -ef|grep $PORT|grep -v grep|wc -l`
+
+echo "parameter:$SERVICE_NAME"
+count=`ps -ef|grep $SERVICE_PORT|grep -v grep|wc -l`
 
 echo "Current process count:$count"
 if [ 0 == $count ];then
     echo "Start app Restart"
-    cd /$NAME
+    cd /$SERVICE_DIR
 
     python main.py
     echo "End API Restart"
   else
-    ID=`ps -ef | grep $PORT| grep -v "grep" | awk '{print $2}'|head -1`
+    ID=`ps -ef | grep $SERVICE_PORT| grep -v "grep" | awk '{print $2}'|head -1`
     echo "Current process id :$ID"
     echo "Start Kill API Process..."
     for id in $ID
@@ -30,7 +32,7 @@ if [ 0 == $count ];then
     sleep 3
     echo "Start API Restart..."
     echo "Please Wait..."
-    cd /$NAME
+    cd /$SERVICE_DIR
     python main.py
     echo "End API Restart"
 fi
